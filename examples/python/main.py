709eb809575075bf18c5ad7178b1c293bec4b3f3
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-11-09 17:24:30 +1100

    introduce linting errs to python example code

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,5 +1,11 @@
+# This file has the following linting errors:
+# - inline comment too close to source code
+# - too many lines between code
+
+
 def main(count):
-    print("Hello world " * count)  # this comment should be two spaces from end of code
+    print("Hello world " * count) # this comment should be two spaces from end of code
+
 
 
 if __name__ == "__main__":
