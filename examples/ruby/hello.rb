255a582df3720c366c74c67dc5cc3c8683ad93c9
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-11-10 22:27:22 +1100

    fix a couple of the lint problems with ruby code, but not all

--- examples/ruby/hello.rb
+++ examples/ruby/hello.rb
@@ -1,12 +1,14 @@
+
+# This shiny device says hello
 class HelloWorld
   def initialize(name)
     @name = name.capitalize
   end
-  def sayHi
+  def say_hi
     puts "Hello #{@name}!"
     puts "My name is Jono Belotti"
   end
 end
 
 hello = HelloWorld.new("World")
-hello.sayHi
+hello.say_hi
