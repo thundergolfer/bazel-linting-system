c8c584cf0706493c32a4d6ca7e907d97fd617956
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-01-12 19:41:01 +1100

    got 'bazel run //typescript/src/demo:devserver' working 🎊

---
+++ examples/typescript/src/demo/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Snackbar from "examples/typescript/src/lib/Snackbar/Snackbar";
+
+interface ISnackbarDemoState {
+  showSnackbar: boolean;
+}
+export default class SnackbarDemo extends React.Component<
+  {},
+  ISnackbarDemoState
+> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      showSnackbar: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              showSnackbar: !this.state.showSnackbar
+            });
+          }}
+        >
+          Toggle Snackbar
+        </button>
+        {this.renderSnackbar()}
+      </div>
+    );
+  }
+
+  renderSnackbar() {
+    return this.state.showSnackbar ? <Snackbar>hello,bazel!</Snackbar> : null;
+  }
+}
