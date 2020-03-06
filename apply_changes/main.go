9819efa5f773cb5778bdba4c7948943726812494
Jonathon Belotti <jonathon@canva.com>
2020-03-06 22:40:27 +1100

    progress on basic functionality of go program

--- apply_changes/main.go
+++ apply_changes/main.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
+func CleanTargetName(uncleanTarget string) string {
+	return strings.Replace(uncleanTarget, "//", "", -1)
+}
+
 func main() {
+	args := os.Args
+
+	if len(args) < 4 {
+		fmt.Fprintf(os.Stderr,"usage: <bazel-repo-root> $(bazel info bazel-genfiles) [targets ...]")
+		os.Exit(1)
+	}
+
+	repoRoot := args[1]
+	genfilesRoot := args[2]
+	targets := args[3:]
+
 	fmt.Println("Hello World")
+	fmt.Printf("repoRoot: %s\n", repoRoot)
+	fmt.Printf("genfilesRoot: %s\n", genfilesRoot)
+	fmt.Printf("targets: %s\n", targets)
+
+	for _, target := range targets {
+		fmt.Printf("clean target: %s\n", CleanTargetName(target))
+	}
 }
 
