b73da83173d8f20f3c3f22f3a651699f56a8fdd7
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2020-03-07 16:31:36 +1100

    refactor for better logging and error handling

--- apply_changes/main.go
+++ apply_changes/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
 )
+
+type SimpleLog struct {
+	DebugEnabled bool
+}
+
+func (m *SimpleLog) Debug(args ...interface{}) {
+	if m.DebugEnabled {
+		log.Println(args...)
+	}
+}
+
+func (m *SimpleLog) Info(args ...interface{}) {
+	log.Println(args...)
+}
 
 
 type target struct {
@@ -47,8 +62,6 @@ func CleanTargetName(uncleanTarget string) string {
 func OverwriteFilesWithLintedVersions(repoRoot, dir string) error {
 	var files []string
 
-	fmt.Println("In overwrite stage...")
-
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if !(info.IsDir()) {
 			files = append(files, path)
@@ -63,37 +76,72 @@ func OverwriteFilesWithLintedVersions(repoRoot, dir string) error {
 		absoluteFilepath := path.Join(repoRoot, repoRelativeFilepath)
 		_, err := copy(file, absoluteFilepath)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 	}
 	return nil
 }
 
-func ParseTargetName(t string) *target {
+func ParseTargetName(t string) (*target, error) {
 	cleanTarget := CleanTargetName(t)
 	parts := strings.Split(cleanTarget, ":")
 	if len(parts) == 1 {
 		return &target{
 			bzlPackage: "",
 			name: parts[0],
-		}
+		}, nil
 	} else if len(parts) == 2 {
 		return &target{
 			bzlPackage: parts[0],
 			name: parts[1],
+		}, nil
+	}
+	err := fmt.Errorf("received invalid label: %s", t)
+	return nil, err
+}
+
+func ApplyLintedChanges(logger SimpleLog, repoRoot, genfilesRoot, target string) error {
+	parsedTarget, err := ParseTargetName(target)
+	if err != nil {
+		return err
+	}
+
+	lintedFilesDir := path.Join(
+		genfilesRoot,
+		parsedTarget.bzlPackage,
+		"__linting_system",
+		parsedTarget.name)
+
+	fileInfo, err := os.Stat(lintedFilesDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			logger.Debug(fmt.Sprintf(
+				"The directory '%s' does not exist so no linted file for target '%s/%s'\n",
+				lintedFilesDir,
+				parsedTarget.bzlPackage,
+				parsedTarget.name,
+			))
+			return nil
+		} else {
+			return err
+		}
+	} else if fileInfo.IsDir() {
+		err = OverwriteFilesWithLintedVersions(repoRoot, lintedFilesDir)
+		if err != nil {
+			return err
 		}
 	}
-	fmt.Fprintf(os.Stderr, "Received invalid label: %s", t)
-	os.Exit(1)
 	return nil
 }
 
 func main() {
 	args := os.Args
 
+	logger := SimpleLog{DebugEnabled: false}
+
 	if len(args) < 4 {
-		fmt.Fprintf(os.Stderr,"usage: <bazel-repo-root> $(bazel info bazel-genfiles) [targets ...]")
+		fmt.Println("usage: <bazel-repo-root> $(bazel info bazel-genfiles) [targets ...]")
 		os.Exit(1)
 	}
 
@@ -103,31 +151,10 @@ func main() {
 	targets := strings.Split(targetsGroup, " ")
 
 	for _, target := range targets {
-		parsedTarget := ParseTargetName(target)
-
-		lintedFilesDir := path.Join(
-			genfilesRoot,
-			parsedTarget.bzlPackage,
-			"__linting_system",
-			parsedTarget.name)
-
-		fileInfo, err := os.Stat(lintedFilesDir)
+		err := ApplyLintedChanges(logger, repoRoot, genfilesRoot, target)
 		if err != nil {
-			if os.IsNotExist(err) {
-				_, _ = fmt.Fprintf(
-					os.Stderr,
-					"The directory '%s' does not exist so no linted file for target '%s/%s'\n",
-					lintedFilesDir,
-					parsedTarget.bzlPackage,
-					parsedTarget.name,
-				)
-				continue
-			} else {
-				// TODO(Jonathon): Don't panic
-				panic(err)
-			}
-		} else if fileInfo.IsDir() {
-			_ = OverwriteFilesWithLintedVersions(repoRoot, lintedFilesDir)
+			fmt.Printf("Error on target '%s': %s", target, err)
+			os.Exit(1)
 		}
 	}
 }
